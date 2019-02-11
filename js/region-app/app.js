7f6ad1dc47598c722796cd2723432c209c03c915
= <matt.0609@hotmail.com>
2019-02-11 10:55:41 -0500

    Slight refactor

--- js/region-app/app.js
+++ js/region-app/app.js
@@ -5,8 +5,17 @@ import { Canvas } from './canvas.js'
 const app = new Interact(document.getElementById('app'))
 const draw = new Canvas(app.element)
 
-// Create regions
+// Create regions and set a threshold
 app.createRegions(12)
+app.threshold = 45
+
+// Pass the events object your on-event functions 
+// (lifecycle: start -> move -> end)
+app.events = {
+    start: onstart,
+    move: onmove,
+    end: onend
+}
 
 // Display region information
 function info() {
@@ -14,32 +23,23 @@ function info() {
         `Region: ${app.circularThreshold() && app.currentRegion}`
 }
 
-// Set threshold
-app.threshold = 45
-
-
-// Pass the events object your on-event functions (start -> move -> end)
-app.events = {
-    start: onstart,
-    move: onmove,
-    end: onend
-}
-
+// Start event
 function onstart() {
     draw.regions(150, app)
     draw.circle(app.initial, 9, '#6B9F55')
     info()
 }
 
+// Move event
 function onmove() {
     info()
 }
 
+// End event
 function onend() {
     info()
     if (app.circularThreshold()) draw.circle(app.current, 6, '#9F5555')
 }
-
 
 // Initial info
 info();
